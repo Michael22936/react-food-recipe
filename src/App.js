e153ea91da1ce144083af25cb9a8bf08d1894521
Michael McDonnell <Michael.McDonnell067@gmail.com>
2022-06-09 21:23:41 -0500

    removed unnessasery console log

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ function App() {
 		const response = await  fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
 		const data = await response.json();
 		setRecipes(data.hits);
-		console.log(data.hits);
 	}
 
 	const updateSearch = e => {
